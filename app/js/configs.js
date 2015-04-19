c57f95e7e1eddda43b732ec40065eebe37c206b9
Matteo Biagetti <m.biagetti@dnsee.com>
2015-04-19 15:35:24 +0200

    fix typo in config

--- app/js/configs.js
+++ app/js/configs.js
@@ -1,5 +1,5 @@
 acmeSocialApp
     .constant('ENDPOINT', {
-        POSTS :  "http://fe.biagetti.info/api/posts",
-        AUTHORS: "http://fe.biagetti.info/api/authors"
+        POSTS :  "http://be.biagetti.info/api/posts",
+        AUTHORS: "http://be.biagetti.info/api/authors"
     });
